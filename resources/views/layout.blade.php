0eddaf4440eb35c94d2abd0b443512903b8c82f0
Joseph Szymborski <joseph.szymborski@mail.mcgill.ca>
2020-04-19 12:59:32 -0400

    incremented version number

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -17,7 +17,7 @@
 
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="/">DreamQuiz <small>v0.4</small></a>
+            <a class="navbar-brand" href="/">DreamQuiz <small>v0.5</small></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
